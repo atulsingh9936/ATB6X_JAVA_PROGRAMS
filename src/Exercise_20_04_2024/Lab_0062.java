5508efd1a500c31257dc93fc83a4c8452dc75672
atulsingh9936 <atul.singh2189@gmail.com>
2024-04-20 23:17:39 +0530

    fourth commit

---
+++ src/Exercise_20_04_2024/Lab_0062.java
@@ -0,0 +1,8 @@
+package Exercise_20_04_2024;
+
+public class Lab_0062 {
+    public static void main(String[] args) {
+      //  int a = new int(8);
+      //  new operator is only available to non primitive types - string, array, class
+    }
+}
