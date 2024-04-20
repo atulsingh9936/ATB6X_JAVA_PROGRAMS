0bf27f76ea75c029746738051927646cecac7df1
atulsingh9936 <atul.singh2189@gmail.com>
2024-04-20 13:51:11 +0530

    third commit

---
+++ src/excercise_18042024/Lab_0028.java
@@ -0,0 +1,15 @@
+package excercise_18042024;
+
+public class Lab_0028 {
+    public static void main(String[] args) {
+
+        // arithmetic operator
+        int a=10;
+        int b =20;
+        System.out.println(b+a);
+        System.out.println(b-a);
+        System.out.println(b*a);
+        System.out.println(b/a);
+        System.out.println(b%a);// print  modulus-remainder
+    }
+}
---
+++ src/excercise_18042024/Lab_0041.java
@@ -0,0 +1,10 @@
+package excercise_18042024;
+
+public class Lab_0041 {
+    public static void main(String[] args) {
+        // Casting -? -molding
+        byte b =10;
+      //  int a = b;   // Implicit casting - jvm will do automatically ( you dont have to specify)
+        int a = (int) b;   // Explicit casting ->jvm
+    }
+}
