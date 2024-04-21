41da78a6d2fff4ffe7f94a5fb627c37b509972fa
atulsingh9936 <atul.singh2189@gmail.com>
2024-04-21 12:15:21 +0530

    assignment

---
+++ src/java_home_work/Question_2.java
@@ -0,0 +1,16 @@
+package java_home_work;
+
+public class Question_2 {
+    public static void main(String[] args) {
+
+
+        int a=10;
+        //  ++a -> result =11, a=11
+        // a++  ->  result = 11, a=12,
+        // a++   -> result =12, a=13
+
+        System.out.println(++a + a++ + a++);
+
+    }
+    }
+
