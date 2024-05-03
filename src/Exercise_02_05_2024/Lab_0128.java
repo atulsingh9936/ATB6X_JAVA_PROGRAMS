6f53c163c53afac6ee81c4f2ad89202345fc6a75
atulsingh9936 <atul.singh2189@gmail.com>
2024-05-03 23:59:29 +0530

    01_05_2024

---
+++ src/Exercise_02_05_2024/Lab_0128.java
@@ -0,0 +1,18 @@
+package Exercise_02_05_2024;
+
+public class Lab_0128 {
+    public static void main(String[] args) {
+        //{}, new operator
+
+        int a[] ={1,7,9,5};
+        System.out.println(a.length);
+       // int[] a1 ={7,8,9,8};
+
+        // how to print all the elements
+        //  loop
+
+        for(int i =0;i<a.length; i++){  //0,1,2,3
+            System.out.println(a[i]);
+        }
+    }
+}
